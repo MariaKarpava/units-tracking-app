28f878fb5c7bb705203bd0641bbc7f72724a7579
MariaKarpava <mary.talabaeva@gmail.com>
2023-07-03 12:04:39 +0100

    Create new project unit-tracking-app.

---
+++ units-tracking-app/units-tracking-app/units_tracking_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  units_tracking_appApp.swift
+//  units-tracking-app
+//
+//  Created by Maryia Karpava on 03/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct units_tracking_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
