c2e3a80c099dd5523ce537be08585a18dc1b7eb2
MariaKarpava <mary.talabaeva@gmail.com>
2023-08-16 16:40:36 +0100

    Add GoalsService.

---
+++ units-tracking-app/units-tracking-app/BusinessLayer/ GoalsService.swift
@@ -0,0 +1,47 @@
+//
+//   GoalsService.swift
+//  units-tracking-app
+//
+//  Created by Maryia Karpava on 16/08/2023.
+//
+
+import Foundation
+
+
+class  GoalsService: ObservableObject {
+    func getUnitsPerDay() -> Int {
+        return 3
+    }
+    
+    func getUnitsPer7Days() -> Int {
+        return 14
+    }
+    
+    func saveUnitsPerDay() {
+        
+    }
+    
+    func saveUnitsPer7Days() {
+        
+    }
+    
+    func updateUnitsPerDay() {
+        
+    }
+    
+    func updateUnitsPer7Days() {
+        
+    }
+    
+    func deleteUnitsPerDay() {
+        
+    }
+    
+    func deleteUnitsPer7Days(){
+        
+    }
+    
+    func getInfo() {
+        
+    }
+}
