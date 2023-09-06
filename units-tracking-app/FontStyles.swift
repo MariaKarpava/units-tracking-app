f7737ecfadd30cc877121f49d968e09d1ad22b51
MariaKarpava <mary.talabaeva@gmail.com>
2023-09-06 19:00:44 +0100

    Create basic home screen. Add colors and fonts.

---
+++ units-tracking-app/FontStyles.swift
@@ -0,0 +1,19 @@
+//
+//  FontStyles.swift
+//  units-tracking-app
+//
+//  Created by Maryia Karpava on 06/09/2023.
+//
+
+import Foundation
+import SwiftUI
+
+
+extension Font {
+    static var homeScreenUnits: Font {
+        Font.custom("Inter-Bold", size: 120)
+    }
+    static var homeScreenText: Font {
+        Font.custom("Inter-Bold", size: 20)
+    }
+}
---
+++ units-tracking-app/units-tracking-app/PresentationLayer/HomeViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  HomeView.swift
+//  units-tracking-app
+//
+//  Created by Maryia Karpava on 06/09/2023.
+//
+
+import Foundation
+import SwiftUI
+
