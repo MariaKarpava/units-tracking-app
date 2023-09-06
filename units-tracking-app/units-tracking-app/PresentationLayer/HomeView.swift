8784d5df2dce0a1ec8cecdc3314afef4756bbf03
MariaKarpava <mary.talabaeva@gmail.com>
2023-09-06 19:00:44 +0100

    Create basic home screen. Add colors and fonts.

---
+++ units-tracking-app/units-tracking-app/PresentationLayer/HomeView.swift
@@ -0,0 +1,36 @@
+//
+//  HomeView.swift
+//  units-tracking-app
+//
+//  Created by Maryia Karpava on 06/09/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    init() {
+        for fontFamily in UIFont.familyNames {
+            print(fontFamily)
+        }
+    }
+    
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            Spacer()
+            Text("12.5").font(.largeTitle).bold()
+            Text("Units remaining for today").font(.title2)
+            Spacer()
+            Text("Why?").underline()
+            Spacer()
+            Spacer()
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
