db1d814a86c8365253f9e6a20fa71336eb60c767
MariaKarpava <mary.talabaeva@gmail.com>
2023-09-11 14:42:56 +0100

    Change getUnitsPerDay and getUnitsPer7Days in GoalsService from functions to computed properties.

--- units-tracking-app/units-tracking-app/BusinessLayer/ GoalsService.swift
+++ units-tracking-app/units-tracking-app/BusinessLayer/ GoalsService.swift
@@ -10,12 +10,12 @@ import Foundation
 
 class  GoalsService: ObservableObject {
     /// Returns the number of units per day which is the max value but still within a limit.
-    func getUnitsPerDay() -> Double {
+    var getUnitsPerDay: Double {
         return 3.0
     }
     
     /// Returns the number of units per 7 days which is the max value but still within a limit.
-    func getUnitsPer7Days() -> Double {
+    var getUnitsPer7Days: Double {
         return 14.0
     }
 }
