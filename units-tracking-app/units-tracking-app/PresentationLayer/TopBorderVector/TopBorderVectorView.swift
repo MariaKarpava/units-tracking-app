3b88d3624f00652f18c503a815900f1f240bcf0d
MariaKarpava <mary.talabaeva@gmail.com>
2023-09-22 20:38:51 +0100

    Add more accurate customization of tab bar vector.

--- units-tracking-app/units-tracking-app/PresentationLayer/TopBorderVector/TopBorderVectorView.swift
+++ units-tracking-app/units-tracking-app/PresentationLayer/TopBorderVector/TopBorderVectorView.swift
@@ -10,69 +10,121 @@ import CoreGraphics
 
 
 struct TopBorderVectorView: View {
-    
     var body: some View {
         GeometryReader { geometry in
-            let offsetY: CGFloat = 11
-            let offsetX: CGFloat = 1
-            
-            let startFirstLine = CGPoint(x: 0, y: 27-offsetY)
-            let endFirstLine = CGPoint(x: 142, y: 27-offsetY)
-            
-            let startSecondLine = CGPoint(x: 248, y: 27-offsetY)
-            let endSecondLine = CGPoint(x: 360, y: 27-offsetY)
-            
-            // First horizontal line
-            Path { path in
-                path.move(to: startFirstLine)
-                path.addLine(to: endFirstLine)
-            }
-            .stroke(lineWidth: 0.1)
-            .foregroundColor(.black)
-            
-            // First connection between arc and line
-            Path { path in
-                path.move(to: CGPoint(x: 141+offsetX, y: 27-offsetY)) // можно регyлировать по x
+            FirstLine()
+            FirstCurve()
+            Arc()
+            SecondCurve()
+            SecondLine()
+        }.frame(width: 390, height: 27)
+    }
+}
+
+
+struct FirstLine: View {
+    var body: some View {
+        let offsetY: CGFloat = 11
+        
+        let startFirstLine = CGPoint(x: 0, y: 27-offsetY)
+        let endFirstLine = CGPoint(x: 144.5, y: 27-offsetY-0.05)
+        
+        Path { path in
+            path.move(to: startFirstLine)
+            path.addLine(to: endFirstLine)
+        }
+        .stroke(lineWidth: 0.2)
+        .foregroundColor(.black)
+    }
+}
+
+struct FirstCurve: View {
+    var body: some View {
+        let offsetY: CGFloat = 11
+        let offsetX: CGFloat = 3.5
+        
+        Path { path in
+            path.move(to: CGPoint(x: 141+offsetX, y: 27-offsetY)) // можно регyлировать по x
+            path.addQuadCurve(
+                to: CGPoint(x: 156.5+offsetX-0.3, y: 15-offsetY+0.3), // точно правильно
+                control: CGPoint(x: 152+offsetX, y: 26-offsetY)
+            )
+        }
+        .stroke(lineWidth: 0.2)
+        .foregroundColor(.black)
+    }
+}
+
+
+struct Arc: View {
+    var body: some View {
+        Path { path in
+            path.addArc(
+                center: CGPoint(x: 195, y: 15+6),
+                radius: 39,
+                startAngle: .degrees(0 - 25),
+                endAngle: .degrees(180 + 25),
+                clockwise: true)
+        }
+        .stroke(lineWidth: 0.2)
+        .foregroundColor(.black)
+    }
+}
+
+struct SecondCurve: View {
+    var body: some View {
+        let offsetY: CGFloat = 11
+        let offsetX: CGFloat = 3.5
+        
+        Path { path in
+                path.move(to: CGPoint(x: 141 + offsetX, y: 27 - offsetY))
                 path.addQuadCurve(
-                    to: CGPoint(x: 156.5+offsetX, y: 15-offsetY), // точно правильно
-                    control: CGPoint(x: 155+offsetX, y: 26-offsetY)
+                    to: CGPoint(x: 156.5 + offsetX, y: 15 - offsetY),
+                    control: CGPoint(x: 155 + offsetX, y: 26 - offsetY)
                 )
             }
             .stroke(lineWidth: 0.1)
-            .foregroundColor(.red)
-            
-            // Arc
-            Path { path in
-                path.addArc(center: CGPoint(x: 195, y: 15), radius: 39, startAngle: .degrees(0), endAngle: .degrees(180), clockwise: true)
-            }
-            .trim(from: 0, to: 0.92)
-            .trim(from: 0.08, to: 1)
-            .stroke(lineWidth: 0.1)
-            
-            // Second connection between arc and line
-            Path { path in
-                    path.move(to: CGPoint(x: 141 + offsetX, y: 27 - offsetY))
-                    path.addQuadCurve(
-                        to: CGPoint(x: 156.5 + offsetX, y: 15 - offsetY),
-                        control: CGPoint(x: 155 + offsetX, y: 26 - offsetY)
-                    )
-                }
-                .stroke(lineWidth: 0.1)
-                .foregroundColor(.black)
-                .scaleEffect(x: -1, y: 1, anchor: .center)
-            
-            // Second line
-            Path { path in
-                path.move(to: startSecondLine)
-                path.addLine(to: endSecondLine)
-            }
-            .stroke(lineWidth: 0.1)
             .foregroundColor(.black)
-            
-        }.frame(width: 390, height: 27
-        )
+            .scaleEffect(x: -1, y: 1, anchor: .center)
     }
 }
+
+
+struct SecondLine: View {
+    var body: some View {
+        let offsetY: CGFloat = 11
+        
+        let startSecondLine = CGPoint(x: 248, y: 27-offsetY)
+        let endSecondLine = CGPoint(x: 390, y: 27-offsetY)
+        
+        Path { path in
+            path.move(to: startSecondLine)
+            path.addLine(to: endSecondLine)
+        }
+        .stroke(lineWidth: 0.1)
+        .foregroundColor(.black)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 struct TopBorderVectorView_Previews: PreviewProvider {
