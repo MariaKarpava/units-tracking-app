99d72227924f4d1222629c07dd225f52b42652ec
MariaKarpava <mary.talabaeva@gmail.com>
2023-10-16 21:52:50 +0100

    Add Color extensions to avoid "magic strings" and duplication.

---
+++ units-tracking-app/units-tracking-app/PresentationLayer/Color+Custom.swift
@@ -0,0 +1,29 @@
+//
+//  Color+Custom.swift
+//  units-tracking-app
+//
+//  Created by Maryia Karpava on 16/10/2023.
+//
+
+import Foundation
+import SwiftUI
+
+
+extension Color {
+    static var accent: Color {
+        Color("Accent")
+    }
+    
+    static var customOrange: Color {
+        Color("CustomOrange")
+    }
+    
+    static var mainText: Color {
+        Color("MainText")
+    }
+    
+    static var secondaryText: Color {
+        Color("SecondaryText")
+    }
+}
+
