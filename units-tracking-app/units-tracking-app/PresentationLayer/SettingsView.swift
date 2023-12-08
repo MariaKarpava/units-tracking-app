0b619350f6cafd2ad8684ab5c2e1c0fc906403b2
MariaKarpava <mary.talabaeva@gmail.com>
2023-12-08 14:03:26 +0000

    Refactor: add settingCell() instead of separate setting computed properties. Make all cell tappable.

--- units-tracking-app/units-tracking-app/PresentationLayer/SettingsView.swift
+++ units-tracking-app/units-tracking-app/PresentationLayer/SettingsView.swift
@@ -26,12 +26,12 @@ struct SettingsView: View {
             NavigationStack {
                 VStack() {
                     Spacer().frame(height: 50)
-                    DailyLimitView
-                    WeeklyLimitView
-                    InfoWarningsView
+                    settingCell(settingTitle: "Daily limit", settingValue: "unit(s)", showUnits: true)
+                    settingCell(settingTitle: "Weekly limit", settingValue: "unit(s)", showUnits: true)
+                    infoWarningsView
                     Spacer().frame(height: 60)
-                    NextDayStartView
-                    InfoStartOfTheDay
+                    settingCell(settingTitle: "Next day starts at", settingValue: "04:00", showUnits: false)
+                    infoStartOfTheDay
                     Spacer()
                 }
                 .toolbar {
@@ -45,60 +45,34 @@ struct SettingsView: View {
         }
     }
     
-    var DailyLimitView: some View {
-        HStack {
-            Text("Daily limit").foregroundColor(.mainText)
-            Spacer()
-            NavigationLink(destination: LimitView()) {
-                Text(String(format: "%0.1f", settingsViewModel.getDailyLimit()))
-                Text("unit(s)")
-                Text(">")
-            }.foregroundColor(.secondaryText)
-        }
-        .frame(height: 50)
-        .padding(.horizontal, 20)
-        .font(.settingsScreenMainInfo)
-        
-    }
-    
-    var WeeklyLimitView: some View {
-        HStack {
-            Text("Weekly limit").foregroundColor(.mainText)
-            Spacer()
-            NavigationLink(destination: LimitView()) {
-                Text(String(format: "%0.1f", settingsViewModel.getWeeklyLimit()))
-                Text("unit(s)")
-                Text(">")
-            }.foregroundColor(.secondaryText)
+    func settingCell(settingTitle: String, settingValue: String, showUnits: Bool) -> some View {
+        NavigationLink(destination: LimitView()) {
+            HStack {
+                Text("\(settingTitle)").foregroundColor(.mainText)
+                Spacer()
+                HStack {
+                    if showUnits {
+                        Text(String(format: "%0.1f", settingsViewModel.getDailyLimit()))
+                    }
+                    Text("\(settingValue)")
+                    Text(">")
+                }
+                .foregroundColor(.secondaryText)
+            }
         }
         .frame(height: 50)
         .padding(.horizontal, 20)
         .font(.settingsScreenMainInfo)
     }
     
-    var InfoWarningsView: some View {
+    var infoWarningsView: some View {
         Text("You will get warnings if you exceed the limits or get close to do so. These limits do not affect how many units you can log.")
             .font(.settingsScreenWarningInfo)
             .foregroundColor(.mainText)
             .padding(.horizontal, 20)
     }
     
-    var NextDayStartView: some View {
-        HStack {
-            Text("Next day starts at")
-            Spacer()
-            NavigationLink(destination: LimitView()) {
-                Text("04:00")
-                Text(">")
-            }
-            .foregroundColor(.secondaryText)
-        }
-        .frame(height: 50)
-        .padding(.horizontal, 20)
-        .font(.settingsScreenMainInfo)
-    }
-    
-    var InfoStartOfTheDay: some View {
+    var infoStartOfTheDay: some View {
         Text("Units logged before this time tomorrow will be counted as consumed today. \"Today\" means \"before going to sleep\", not \"before midnight\".")
             .font(.settingsScreenWarningInfo)
             .foregroundColor(.mainText)
