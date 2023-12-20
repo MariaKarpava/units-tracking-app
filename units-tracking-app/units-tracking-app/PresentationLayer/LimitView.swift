572ccec63c1589e19aa631f6b7dc9bfc8f0a92a8
MariaKarpava <mary.talabaeva@gmail.com>
2023-12-20 23:35:05 +0000

    Move functions and computed properties to the LimitView extension.

--- units-tracking-app/units-tracking-app/PresentationLayer/LimitView.swift
+++ units-tracking-app/units-tracking-app/PresentationLayer/LimitView.swift
@@ -10,81 +10,65 @@ import SwiftUI
 
 struct LimitView: View {
     @ObservedObject var limitViewModel: LimitViewModel
-    
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
-
-        var customBackButton : some View { 
-            Button(action: {
-            self.presentationMode.wrappedValue.dismiss()
-            }) {
-                HStack {
-                    Image(systemName: "chevron.left")
-                        .foregroundColor(.mainText)
-                Text("")
+    
+    var body: some View {
+        NavigationStack {
+            GeometryReader { geometry in
+                VStack(alignment: .leading) {
+                    header
+                    Spacer()
+                        .frame(height: 60)
+                    infoText
+                    .padding(.horizontal, 55)
+                    Spacer()
+                        .frame(height: 140)
+                    buttonsWithUnits
+                    Spacer()
+                }
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            print("save")
+                        } label: {
+                            Text("Save")
+                                .underline()
+                                .font(.limitScreenSaveButton)
+                                .foregroundColor(.mainText)
+                        }
+                    }
                 }
             }
         }
-        
-    
-    var body: some View {
-            NavigationStack {
-                GeometryReader { geometry in
-                    VStack(alignment: .leading) {
-                        header
-                        Spacer()
-                            .frame(height: 60)
-                        infoText
-                        .padding(.horizontal, 55)
-                        Spacer().frame(height: 140)
-                        HStack {
-                            Spacer()
-                            Button {
-                                limitViewModel.decrementUnits()
-                            } label: {
-                                addCustomStepperButton(sign: "-", color: limitViewModel.viewState.buttonColor)
-                            }
-                            .disabled(!limitViewModel.viewState.unitsAreValid)
-                            unitsIncrementer.frame(width: 100)
-                            Button {
-                                limitViewModel.incrementUnits()
-                            } label: {
-                                addCustomStepperButton(sign: "+", color: .mainText)
-                            }
-                            Spacer()
-                        }
-                        Spacer()
-                    }
-                        
-                    .toolbar {
-                        ToolbarItem(placement: .navigationBarTrailing) {
-                            Button {
-                                print("save")
-                            } label: {
-                                Text("Save")
-                                    .underline()
-                                    .font(.limitScreenSaveButton)
-                                    .foregroundColor(.mainText)
-                            }
-                        }
-                }
-                }
-
-            } 
-            .navigationBarBackButtonHidden(true)
-            .navigationBarItems(leading: customBackButton)
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: customBackButton)
     }
-    
+}
+
+
+extension LimitView {
+    var customBackButton : some View {
+        Button(action: {
+        self.presentationMode.wrappedValue.dismiss()
+        }) {
+            HStack {
+                Image(systemName: "chevron.left")
+                    .foregroundColor(.mainText)
+            Text("")
+            }
+        }
+    }
+
     var header: some View {
         Text(limitViewModel.viewState.header)
             .font(.limitScreenHeader)
             .foregroundColor(.mainText)
             .padding(20)
     }
-
     
     var infoText: some View {
         Text("""
-            According to the NHS, males should 
+            According to the NHS, males should
             not exceed a daily limit of 8 units,
             while for females the value is 6 units. 
             
@@ -103,6 +87,25 @@ struct LimitView: View {
         }.font(.limitScreenUnits)
     }
     
+    var buttonsWithUnits: some View {
+        HStack {
+            Spacer()
+            Button {
+                limitViewModel.decrementUnits()
+            } label: {
+                addCustomStepperButton(sign: "-", color: limitViewModel.viewState.buttonColor)
+            }
+            .disabled(!limitViewModel.viewState.unitsAreValid)
+            unitsIncrementer.frame(width: 100)
+            Button {
+                limitViewModel.incrementUnits()
+            } label: {
+                addCustomStepperButton(sign: "+", color: .mainText)
+            }
+            Spacer()
+        }
+    }
+    
     func addCustomStepperButton(sign: String, color: Color) -> some View {
         RoundedRectangle(cornerRadius: 10)
             .stroke(color, lineWidth: 2)
@@ -113,6 +116,7 @@ struct LimitView: View {
                     .font(.limitScreenStepperSign)
             )
     }
+
 }
 
 
