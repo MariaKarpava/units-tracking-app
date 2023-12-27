b51a3e6f234eac2fae482366449ee4800ed1801f
MariaKarpava <mary.talabaeva@gmail.com>
2023-12-27 09:42:41 +0000

    Get rid of magic strings: dd enums GoalsService.NotificationName and GoalsService.UserDefaultsKey.

--- units-tracking-app/units-tracking-app/BusinessLayer/GoalsService.swift
+++ units-tracking-app/units-tracking-app/BusinessLayer/GoalsService.swift
@@ -7,37 +7,44 @@
 
 import Foundation
 
+// TODO: get rid of magic strings. Add enums `GoalsService.Notification` and `GoalsService.UserDefaultsKey` which would host the constants. Make sure best fitting access modifiers are chosen.
+
 
 class  GoalsService: ObservableObject {
-    // TODO: encapsulation violated. Anyone from outside cabnn change the property,  which makes changeUnitsPerDay(:) not very useful. Make this property read-only. Then get rid of didSet -- we don't need it as we have changeUnitsPerDay(:) already which can host this logic.
-    
-    // TODO: why usgin inline initialization? We don't need it as the plan is to initialize it form uer defaults instead (already implemented).
-    
     /// Returns the number of units per day which is the max value but still within a limit.
     private var unitsPerDay: Double
     
     /// Returns the number of units per 7 days which is the max value but still within a limit.
-    private var unitsPer7Days: Double 
+    private var unitsPer7Days: Double
     
+    enum NotificationName {
+        static let dailyLimitHasChanged = Notification.Name("dailyLimitHasChanged")
+        static let weeklyLimitHasChanged = Notification.Name("weeklyLimitHasChanged")
+    }
+    
+    enum UserDefaultsKey {
+        static let dailyLimitHasChanged = "dailyLimitHasChanged"
+        static let weeklyLimitHasChanged = "weeklyLimitHasChanged"
+    }
     
     init() {
         // Decode the daily limit
-        let savedDailyLimit = UserDefaults.standard.double(forKey: "dailyLimitHasChanged")
+        let savedDailyLimit = UserDefaults.standard.double(forKey: UserDefaultsKey.dailyLimitHasChanged)
         unitsPerDay = savedDailyLimit
         
         // Decode the weekly limit
-        let savedWeeklyLimit = UserDefaults.standard.double(forKey: "weeklyLimitHasChanged")
+        let savedWeeklyLimit = UserDefaults.standard.double(forKey: UserDefaultsKey.weeklyLimitHasChanged)
         unitsPer7Days = savedWeeklyLimit
     }
     
     func changeUnitsPerDay(newValue: Double) {
         unitsPerDay = newValue
         
-        UserDefaults.standard.set(unitsPerDay, forKey: "dailyLimitHasChanged")
-        NotificationCenter.default.post(name: .dailyLimitHasChanged, object: self)
-        print("Daily limit saved and = \(unitsPerDay).")
+        UserDefaults.standard.set(unitsPerDay, forKey: UserDefaultsKey.dailyLimitHasChanged)
+        NotificationCenter.default.post(name: NotificationName.dailyLimitHasChanged, object: self)
     }
     
+    // TODO: FYI: alternative could be to make the property `private(set) public` -- that way it would have been read-only from outside but read-write from the inside. But current approach is also fine.
     func getUnitsPerDay() -> Double {
         unitsPerDay
     }
@@ -45,9 +52,8 @@ class  GoalsService: ObservableObject {
     func changeUnitsPer7Days(newValue: Double) {
         unitsPer7Days = newValue
         
-        UserDefaults.standard.set(unitsPer7Days, forKey: "weeklyLimitHasChanged")
-        NotificationCenter.default.post(name: .weeklyLimitHasChanged, object: self)
-        print("Weekly limit saved and = \(unitsPer7Days).")
+        UserDefaults.standard.set(unitsPer7Days, forKey: UserDefaultsKey.weeklyLimitHasChanged)
+        NotificationCenter.default.post(name: NotificationName.weeklyLimitHasChanged, object: self)
     }
     
     func getUnitsPer7Days() -> Double {
